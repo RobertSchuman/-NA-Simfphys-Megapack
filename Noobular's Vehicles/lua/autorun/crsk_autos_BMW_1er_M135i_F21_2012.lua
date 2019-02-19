bdc9636ad6ffb374d4d56f93e41296e167949219
Robert Schuman <47789001+RobertSchuman@users.noreply.github.com>
2019-02-19 17:30:25 +0100

    Update by Robert Schuman
    Here you go, my version of this vehicle

--- Noobular's Vehicles/lua/autorun/crsk_autos_BMW_1er_M135i_F21_2012.lua
+++ Noobular's Vehicles/lua/autorun/crsk_autos_BMW_1er_M135i_F21_2012.lua
@@ -1,5 +1,7 @@
 local Category = '(( Noobs Vehicles ))'
 
+-- Robert's Modification
+
 local V = { 
 
     Name = 'BMW 1er M135i F21 2012',
@@ -9,16 +11,25 @@ local V = {
     --SpawnOffset = Vector(0,0,0),
 
     Members = {
-     Mass = 1496,
+        ModelInfo = 
+        {
+        Bodygroups = {},
+        Color = Color(255,255,255,255) ,
+        Skin = 0 ,
+        WheelColor = Color(255,255,255,50)
+        },
+
+        Mass = 1496,
+        SpeedoMax = 125,
     --MaxHealth = 1000,
     	--AirFriction = -250,
     --LightsTable = 'jeep', 
 
-    FrontWheelRadius = 14.699999809265,
+    FrontWheelRadius = 14.199999809265,
     RearWheelRadius = 14.699999809265,
 
-    CustomMassCenter = Vector(0,0,0),
-    SeatOffset = Vector(0,0,-2),
+    CustomMassCenter = Vector(0,-2,-6),
+    SeatOffset = Vector(0,0,-4),
     SeatPitch = 0,
 
     --SpeedoMax = 150,
@@ -27,38 +38,38 @@ local V = {
     --RPMGaugeMax = 150,
 
     
-    ModelInfo = {Bodygroups = {},Color = Color(255,255,255,255) ,Skin = 0 ,WheelColor = Color(255,255,255,50)},
+    
 
-    PassengerSeats = { {pos = Vector(5,-16,14), ang = Angle(0,-90,20) } },
-    Backfire =false,
+    PassengerSeats = { {pos = Vector(19,-9,14), ang = Angle(0,0,17) } },
+    Backfire             = false,
     StrengthenSuspension = true,
-    FrontHeight = 9,
-    RearHeight = 9,
+    FrontHeight = 8,
+    RearHeight = 8,
 
-    FrontConstant = 16971,
-    RearConstant = 16971,
-
-
-    FrontDamping = 2640,
-    RearDamping = 2640,
+    FrontConstant  = 43000,
+    RearConstant   = 43000,
 
 
-    FrontRelativeDamping = 1760, 
-    RearRelativeDamping = 1760,
+    FrontDamping = 3000,
+    RearDamping  = 3000,
+
+
+    FrontRelativeDamping = 3000, 
+    RearRelativeDamping = 3000,
 
 
     FastSteeringAngle = 9,
     SteeringFadeFastSpeed = 535,
-    TurnSpeed = 4,
-    MaxGrip = 78,
-    Efficiency = 2,
-    GripOffset = 0,
-    BrakePower = 35,
+    TurnSpeed = 6,
+    MaxGrip = 85,
+    Efficiency = 1,
+    GripOffset = -6,
+    BrakePower = 115,
     IdleRPM = 750,
-    LimitRPM = 6500,
-    PeakTorque = 167.5,
-    PowerbandStart = 1200, 
-    PowerbandEnd = 5800,
+    LimitRPM = 7500,
+    PeakTorque = 330,
+    PowerbandStart = 3000, 
+    PowerbandEnd = 6500,
     --Revlimiter = true,
     Turbocharged = false,
     --snd_blowoff = , 
@@ -69,19 +80,19 @@ local V = {
     --DoNotStall = false, 
     PowerBias = 1,	
     EngineSoundPreset = -1,
-    snd_pitch = 1,
-    snd_idle = "simulated_vehicles/4banger/4banger_idle.wav",
+    snd_pitch = 0.8,
+    snd_idle = "vehicles/tdmcars/bmw1m/idle.wav",
 
-    snd_low = "simulated_vehicles/4banger/4banger_low.wav",
+    snd_low = "vehicles/tdmcars/bmw1m/rev.wav",
     snd_low_pitch = 1,
 
-    snd_mid = "simulated_vehicles/4banger/4banger_mid.wav",
-Sound_MidVolume = 2,
-    snd_mid_gearup = "simulated_vehicles/4banger/4banger_second.wav",
+    snd_mid = "vehicles/tdmcars/bmw1m/first.wav",
+    Sound_MidVolume = 2,
+    snd_mid_gearup = "vehicles/tdmcars/bmw1m/third.wav",
     snd_mid_pitch = 1,
     snd_horn = 'simulated_vehicles/horn_1.wav', 
-    DifferentialGear = 0.8,
-    Gears = {-0.1,0,0.1,0.2,0.3,0.4,0.5,0.6}
+    DifferentialGear = 0.5,
+    Gears = {-0.1,0,0.13,0.24,0.36,0.46,0.55}
     }
 }
 list.Set( 'simfphys_vehicles', 'simfphys_NV_crsk_autos_m135i_f21_2012', V ) 
